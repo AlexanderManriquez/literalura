ea3cee83d49a7f5816d67408d3a8c5d1a315299c
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-06-10 21:12:13 -0400

    update Libros.java

--- src/main/java/com/aluracursos/literalura/model/Libros.java
+++ src/main/java/com/aluracursos/literalura/model/Libros.java
@@ -5,4 +5,47 @@ public class Libros {
     private String autores;
     private String idiomas;
     private Long numeroDescargas;
+
+    public Libros(){}
+    
+    public Libros(String titulo, String autores, String idiomas, Long numeroDescargas) {
+        this.titulo = titulo;
+        this.autores = autores;
+        this.idiomas = idiomas;
+        this.numeroDescargas = numeroDescargas;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutores() {
+        return autores;
+    }
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+    public String getIdiomas() {
+        return idiomas;
+    }
+    public void setIdiomas(String idiomas) {
+        this.idiomas = idiomas;
+    }
+    public Long getNumeroDescargas() {
+        return numeroDescargas;
+    }
+    public void setNumeroDescargas(Long numeroDescargas) {
+        this.numeroDescargas = numeroDescargas;
+    }
+    @Override
+    public String toString() {
+        return "Libros [titulo=" + titulo + ", autores=" + autores + ", idiomas=" + idiomas + ", numeroDescargas="
+                + numeroDescargas + "]";
+    }
+
+    
+
+    
 }
