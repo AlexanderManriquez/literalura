e9a39a5760802624bc6c3f206d197216c2e0b2be
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-06-14 17:36:49 -0400

    se actualiza clase Principal.java

--- src/main/java/com/aluracursos/literalura/principal/Principal.java
+++ src/main/java/com/aluracursos/literalura/principal/Principal.java
@@ -1,5 +1,53 @@
 package com.aluracursos.literalura.principal;
 
+import java.util.Scanner;
+
+import com.aluracursos.literalura.model.DatosLibros;
+import com.aluracursos.literalura.services.ConsumoAPI;
+
 public class Principal {
-    
+    private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+    private final String URL_BASE = "https://gutendex.com/books/";
+
+
+    public void mostrarMenu(){
+        var opcion = -1;
+        while (opcion != 0){
+                var menu = """  
+                                1 - Buscar libros
+
+                                0 - Salir
+
+                                Elige una opción:
+                                """;
+                System.out.println(menu);
+                opcion = teclado.nextInt();
+                teclado.nextLine();
+
+                switch (opcion) {
+                        case 1:
+                                buscarLibro();
+                                break;
+                        case 0:
+                                System.out.println("Cerrando la aplicación...");
+                                break;
+                        default:
+                                System.out.println("Opción no válida");
+                        }
+                }
+        }
+
+        private DatosLibros getDatosLibros() {
+            System.out.println("Escribe el id del libro a agregar: ");
+            var idLibro = teclado.nextLine();
+            var json = consumoApi.obtenerDatos(URL_BASE + idLibro);
+            System.out.println(json);
+            DatosLibros datos = conversor.obtenerDatos(json, DatosLibros.class);
+            return datos;
+    }
+
+    private void buscarLibro() {
+        
+    }
 }
