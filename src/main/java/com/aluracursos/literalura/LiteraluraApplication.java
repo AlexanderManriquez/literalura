6628acdc319118e36c68ca8930be88070c10b708
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-06-14 18:15:47 -0400

    se actualiza public class LiteraluraApplication

--- src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
+++ src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
@@ -1,13 +1,30 @@
 package com.aluracursos.literalura;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aluracursos.literalura.principal.Principal;
+import com.aluracursos.literalura.services.ConsumoAPI;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner{
+
+	 @Autowired
+    private ConsumoAPI consumoApi;
+
+    @Autowired
+    private LibrosService librosService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+ 	public void run(String... args) throws Exception {
+ 		Principal principal = new Principal(repository);
+		principal.mostrarMenu();
+	}
+		
 }
