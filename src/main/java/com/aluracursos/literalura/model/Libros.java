07a17bf2eb99d807c9329431f661380153ec00bb
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-06-14 17:51:30 -0400

    se actualiza clase Libros.java

--- src/main/java/com/aluracursos/literalura/model/Libros.java
+++ src/main/java/com/aluracursos/literalura/model/Libros.java
@@ -7,7 +7,7 @@ public class Libros {
     String titulo;
     List<Autor> autor;
     List<String> idiomas;
-    int descargas;
+    int numeroDescargas;
 
     public int getId() {
         return id;
@@ -15,42 +15,38 @@ public class Libros {
     public void setId(int id) {
         this.id = id;
     }
+
     public String getTitulo() {
         return titulo;
     }
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+
     public List<Autor> getAutor() {
         return autor;
     }
     public void setAutor(List<Autor> autor) {
         this.autor = autor;
     }
+
     public List<String> getIdiomas() {
         return idiomas;
     }
     public void setIdiomas(List<String> idiomas) {
         this.idiomas = idiomas;
     }
-    public int getDescargas() {
-        return descargas;
-    }
-    public void setDescargas(int descargas) {
-        this.descargas = descargas;
-    }
     
+    public int getNumeroDescargas() {
+        return numeroDescargas;
+    }
+    public void setNumeroDescargas(int numeroDescargas) {
+        this.numeroDescargas = numeroDescargas;
+    }
+
     @Override
     public String toString() {
-        return "Libros [id=" + id + ", titulo=" + titulo + ", autor=" + autor + ", idiomas=" + idiomas + ", descargas="
-                + descargas + "]";
-    }
-
-    
-
-    
-    
-    
-    
-    
+        return "Libros [id=" + id + ", titulo=" + titulo + ", autor=" + autor + ", idiomas=" + idiomas
+                + ", numeroDescargas=" + numeroDescargas + "]";
+    } 
 }
