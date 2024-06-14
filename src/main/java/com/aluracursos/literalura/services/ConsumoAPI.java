3e39e00bfbc1a51c021f3304d830ea0e5d561352
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-06-14 18:29:44 -0400

    se actualiza consumoAPI.java

--- src/main/java/com/aluracursos/literalura/services/ConsumoAPI.java
+++ src/main/java/com/aluracursos/literalura/services/ConsumoAPI.java
@@ -6,6 +6,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ConsumoAPI {
 
     public String obtenerDatos(String url) {
