f30771c864e5c2da69bf3b55a6d29ee1b3d8e3dc
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-06-14 18:30:42 -0400

    se actualiza LibrosService.java

--- src/main/java/com/aluracursos/literalura/services/LibrosService.java
+++ src/main/java/com/aluracursos/literalura/services/LibrosService.java
@@ -1,9 +1,11 @@
 package com.aluracursos.literalura.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.aluracursos.literalura.model.Libros;
 
+@Service
 public class LibrosService {
     @Autowired
     private LibrosRepository librosRepository;
