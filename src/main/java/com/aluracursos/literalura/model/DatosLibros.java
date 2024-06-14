2428a3cf1cc0c6d20d925a13c3061a80b40a6769
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-06-14 17:55:18 -0400

    se actualiza clase Libros.java

--- src/main/java/com/aluracursos/literalura/model/DatosLibros.java
+++ src/main/java/com/aluracursos/literalura/model/DatosLibros.java
@@ -1,13 +1,15 @@
 package com.aluracursos.literalura.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosLibros(
             @JsonAlias("title") String titulo,
-            @JsonAlias("authors") String autores,
-            @JsonAlias("languages") String idiomas,
+            @JsonAlias("authors") List<Autor> autores,
+            @JsonAlias("languages") List<String> idiomas,
             @JsonAlias("download_count") Long numeroDescargas) {
 
 }
