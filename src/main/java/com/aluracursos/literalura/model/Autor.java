d4a4417502ad6ff72f62c96c70db685bf12934c6
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-06-14 17:54:16 -0400

    se actualiza clase Autor.java

--- src/main/java/com/aluracursos/literalura/model/Autor.java
+++ src/main/java/com/aluracursos/literalura/model/Autor.java
@@ -5,24 +5,36 @@ public class Autor {
     int fechaNacimiento;
     int fechaFallecimiento;
 
+    public Autor(String nombre, int fechaNacimiento, int fechaFallecimiento){
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.fechaFallecimiento = fechaFallecimiento;
+    }
+
     public String getNombre() {
         return nombre;
     }
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public int getFechaNacimiento() {
         return fechaNacimiento;
     }
+
     public void setFechaNacimiento(int fechaNacimiento) {
         this.fechaNacimiento = fechaNacimiento;
     }
+
     public int getFechaFallecimiento() {
         return fechaFallecimiento;
     }
+
     public void setFechaFallecimiento(int fechaFallecimiento) {
         this.fechaFallecimiento = fechaFallecimiento;
     }
+
     @Override
     public String toString() {
         return "Autor [nombre= " + nombre + ", fechaNacimiento= " + fechaNacimiento + ", fechaFallecimiento= "
