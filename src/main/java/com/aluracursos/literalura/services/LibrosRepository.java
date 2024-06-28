809d272a293c3e228e3d2653821e8767812661d6
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-06-28 13:35:46 -0400

    se actualiza LibrosRepository

--- src/main/java/com/aluracursos/literalura/services/LibrosRepository.java
+++ src/main/java/com/aluracursos/literalura/services/LibrosRepository.java
@@ -1,9 +1,11 @@
 package com.aluracursos.literalura.services;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aluracursos.literalura.model.Libros;
 
 public interface LibrosRepository extends JpaRepository<Libros, Integer> {
-
+    List<Libros> findByTituloContainingIgnoreCase(String titulo);
 }
